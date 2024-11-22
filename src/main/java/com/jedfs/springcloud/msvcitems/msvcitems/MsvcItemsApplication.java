f3d7baa5078c9446527abf354daffd0f18802900
Edison Feria <edisonferia4@gmail.com>
2024-11-22 14:52:39 -0500

    primer commit

---
+++ src/main/java/com/jedfs/springcloud/msvcitems/msvcitems/MsvcItemsApplication.java
@@ -0,0 +1,13 @@
+package com.jedfs.springcloud.msvcitems.msvcitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcItemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcItemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jedfs/springcloud/msvcitems/msvcitems/MsvcItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jedfs.springcloud.msvcitems.msvcitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcItemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
